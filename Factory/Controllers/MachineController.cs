e6754bcaa82b91c3253bc52d7ac5499c8b616016
object-ions <moshikoatia@gmail.com>
2023-10-14 12:55:33 -0700

    Update Details in MachineControllers and its corresponding View. Add Create in MachineController. WIP

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -20,5 +20,31 @@ namespace Factory.Controllers
     {
       return View(_db.Machines.ToList());
     }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines.Include(m => m.JoinEntities).ThenInclude(j => j.Machine).FirstOrDefault(m => m.MachineId == id);
+      return View(thisMachine);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(machine);
+      }
+      
+      {
+        _db.Machines.Add(machine);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
   }
 }
