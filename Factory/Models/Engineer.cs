c99150e8dc8461d9491da3d1db3809dff3fad6ab
object-ions <moshikoatia@gmail.com>
2023-10-15 16:49:08 -0700

    update models to requierd fileds

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,7 +6,9 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "Engineer has to have a Name. Please type in.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Engineer has to have a Name. Please type in.")]
     public string Speciality { get; set; }
     public List<EngineerMachine> JoinEntities { get;}
   }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -5,6 +5,7 @@ namespace Factory.Models
   public class Machine
     {
         public int MachineId { get; set; }
+        [Required(ErrorMessage = "Machine has to have a Name. Please type in.")]
         public string Name { get; set; }
         public List<EngineerMachine> JoinEntities { get;}
     }
