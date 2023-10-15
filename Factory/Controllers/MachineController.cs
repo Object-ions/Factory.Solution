7325b9c720caefed536a66de10881fd1c82faffa
object-ions <moshikoatia@gmail.com>
2023-10-15 09:39:10 -0700

    Update a View for Create. Add Controller and View for Edit.

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -46,5 +46,20 @@ namespace Factory.Controllers
         return RedirectToAction("Index");
       }
     }
+
+    public ActionResult Edit(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(m => m.MachineId == id);
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine)
+    {
+      _db.Machines.Update(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
   }
 }
