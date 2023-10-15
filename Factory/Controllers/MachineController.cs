ac4bf01bb678dd081519f7b85cc5146a72553baf
object-ions <moshikoatia@gmail.com>
2023-10-15 10:13:21 -0700

    Add Delete to controllers

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -60,6 +60,20 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    
+
+    public ActionResult Delete(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(m => m.MachineId = id);
+      return View(thisMachine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Machine thisMachine = _db.Machines.FirstOrDefault(m => m.MachineId == id);
+       _db.Machines.Remove(thisMachine);
+       _db.SaveChanges();
+       return RedirectToAction("Index");
+    }
   }
 }
