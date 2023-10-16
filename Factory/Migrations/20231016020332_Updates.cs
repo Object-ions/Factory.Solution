4d3af14d528fa05b744393f26e290eba6328f339
object-ions <moshikoatia@gmail.com>
2023-10-15 19:13:48 -0700

    Update properties after changing the model. WIP

---
+++ Factory/Migrations/20231016020332_Updates.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class Updates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Machines",
+                newName: "Type");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Machines",
+                newName: "Name");
+        }
+    }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -4,10 +4,10 @@ using System.ComponentModel.DataAnnotations;
 namespace Factory.Models
 {
   public class Machine
-    {
-        public int MachineId { get; set; }
-        [Required(ErrorMessage = "Machine has to have a type.")]
-        public string type { get; set; }
-        public List<EngineerMachine> JoinEntities { get;}
-    }
+  {
+      public int MachineId { get; set; }
+      [Required(ErrorMessage = "Machine has to have a type.")]
+      public string Type { get; set; }
+      public List<EngineerMachine> JoinEntities { get;}
+  }
 }
