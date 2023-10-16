4f2c0b9da460cb7bbeaf4788e46cab0a7a9f5a0d
object-ions <moshikoatia@gmail.com>
2023-10-15 21:26:58 -0700

    Update UI

---
+++ Factory/Migrations/20231016041953_RemoveSpecialty.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class RemoveSpecialty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Specialty",
+                table: "Engineers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Specialty",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
